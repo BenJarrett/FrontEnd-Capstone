45d4e2862757eb8fe0231d6348dd416175f06dad
BenJarrett <b.cl.jarrett@gmail.com>
2021-06-14 15:12:13 -0500

    Created axios calls for staff nodules

---
+++ src/helpers/data/staffData.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import firebaseConfig from '../apiKeys';
+
+const dbURL = firebaseConfig.databaseURL;
+
+const getStaffList = () => new Promise((resolve, reject) => {
+  axios.get(`${dbURL}/staff.json`)
+    .then((response) => resolve(Object.values(response.data)))
+    .catch((error) => reject(error));
+});
+
+// const getOnSaleProducts = () => new Promise((resolve, reject) => {
+//   axios.get(`${dbURL}/products.json?orderBy="onSale"&equalTo=true`)
+//     .then((response) => resolve(Object.values(response.data)))
+//     .catch((error) => reject(error));
+// });
+
+// const getChosenStaffPicks = () => new Promise((resolve, reject) => {
+//   axios.get(`${dbURL}/products.json?orderBy="staffPick"&equalTo=true`)
+//     .then((response) => resolve(Object.values(response.data)))
+//     .catch((error) => reject(error));
+// });
+
+const addStaffMember = (staff) => new Promise((resolve, reject) => {
+  axios
+    .post(`${dbURL}/staff.json`, staff)
+    .then((response) => {
+      const body = { firebaseKey: response.data.name };
+      axios
+        .patch(`${dbURL}/staff/${response.data.name}.json`, body)
+        .then(() => {
+          getStaffList().then((staffArray) => resolve(staffArray));
+        });
+    })
+    .catch((error) => reject(error));
+});
+
+const removeStaffMember = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.delete(`${dbURL}/staff/${firebaseKey}.json`)
+    .then(() => getStaffList().then((resp) => resolve(resp)))
+    .catch((error) => reject(error));
+});
+
+const updateStaffMember = (staff) => new Promise((resolve, reject) => {
+  axios.patch(`${dbURL}/staff/${staff.firebaseKey}.json`, staff)
+    .then(() => getStaffList().then(resolve))
+    .catch((error) => reject(error));
+});
+
+export {
+  getStaffList,
+  addStaffMember,
+  removeStaffMember,
+  updateStaffMember
+};
