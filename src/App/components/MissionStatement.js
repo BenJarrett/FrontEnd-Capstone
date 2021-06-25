6be8a336a664d38b20ba1173c853978018731e2a
Ben Jarrett <b.cl.jarrett@gmail.com>
2021-06-25 18:59:44 -0500

    Last minute styling

--- src/App/components/MissionStatement.js
+++ src/App/components/MissionStatement.js
@@ -5,7 +5,7 @@ export default function MissionStatement() {
       <div className="mission-image">
         <div className="mission-text">
           <h2>Our Mission</h2>
-        <h5>Decisive Depot is dedicated to meeting the home importvement needs to all customers. Whether you are a home owner, buisness ownder, or looking for a hobby, we got what you need!</h5>
+        <h5 style= {{ marginLeft: '100px', marginRight: '100px' }}>Decisive Depot is dedicated to meeting the home importvement needs to all customers. Whether you are a home owner, buisness ownder, or looking for a hobby, we got what you need!</h5>
         </div>
       </div>
   );
